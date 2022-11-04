806b2c8a154a85004cbd99d16549c52c93273f2e
yuragvozd2001 <gvozdura715yura@gmail.com>
2022-11-04 15:18:57 +0100

    java script

---
+++ lesson2/javascript2.js
@@ -0,0 +1,166 @@
+let arr = [];
+arr[0] = 1;
+arr[1] = 'man';
+arr[2] = 'woman';
+arr[3] = 'girl';
+arr[4] = 'boy'
+arr[5] = 'grandfather';
+arr[6] = 'grandmother';
+arr[7] = 'cat';
+arr[8] = 'dog';
+arr[9] = 'family';
+arr[10] = true;
+console.log(arr);
+
+let book ={
+    title: 'Creed',
+    pageCount: 10,
+    genre: 'sport',
+}
+console.log(book);
+let book2 ={
+    title: 'Creed 2',
+    pageCount: 30,
+    genre: 'sport',
+}
+console.log(book2);
+let book3 ={
+    title: 'Creed3',
+    pageCount: 60,
+    genre: 'sport',
+}
+console.log(book3);
+
+let book4 ={
+    title: 'Creed',
+    pageCount: 10,
+    genre: 'sport',
+    authors: [
+        {
+            name:'Rocky',
+            age: 30
+        },
+        {
+            name: 'Rocky1',
+            age: 31
+        },
+    ]
+}
+console.log(book4);
+let book5 ={
+    title: 'Creed',
+    pageCount: 30,
+    genre: 'sport',
+    authors: [
+        {
+            name:'Oleg',
+            age: 34
+        },
+        {
+            name: 'Rocky1',
+            age: 35
+        },
+    ]
+}
+console.log(book5);
+let book6 ={
+    title: 'Creed',
+    pageCount: 60,
+    genre: 'sport',
+    authors: [
+        {
+            name:'Ivan',
+            age: 36
+        },
+        {
+            name: 'Rocky1',
+            age: 37
+        },
+    ]
+}
+console.log(book6);
+
+const users = [
+    {
+        name: 'Vlad1',
+        username: 'vlados1',
+        password: 'qwerty1'
+    },
+    {
+        name: 'Vlad2',
+        username: 'vlados2',
+        password: 'qwerty2'
+    },
+    {
+        name: 'Vlad3',
+        username: 'vlados3',
+        password: 'qwerty3'
+    },
+    {
+        name: 'Vlad4',
+        username: 'vlados4',
+        password: 'qwerty4'
+    },
+    {
+        name: 'Vlad5',
+        username: 'vlados5',
+        password: 'qwerty5'
+    },
+    {
+        name: 'Vlad6',
+        username: 'vlados6',
+        password: 'qwerty6'
+    },
+    {
+        name: 'Vlad7',
+        username: 'vlados7',
+        password: 'qwerty7'
+    },
+    {
+        name: 'Vlad8',
+        username: 'vlados8',
+        password: 'qwerty8'
+    },
+    {
+        name: 'Vlad9',
+        username: 'vlados9',
+        password: 'qwerty9'
+    },
+    {
+        name: 'Vlad10',
+        username: 'vlados10',
+        password: 'qwerty10'
+    },
+]
+
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
